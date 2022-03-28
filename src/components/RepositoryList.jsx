605dfadd4f5bb2233f58fd640507fccfab59bd96
Ariel Marte <ariel.amas@hotmail.com>
2022-03-28 18:40:05 -0300

    Environment Configuration and Examples

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,30 @@
+import { RepositoryItem } from "./RepositoryItem"
+
+const repository ={
+    name: 'unform',
+    description: 'Forms in React',
+    link: 'https://github.com/unform/unform'
+}
+
+export function RespositoryList(){
+    return (
+        <section className="repository-list">
+            <h1>Lista de Repositórios</h1>
+
+            <ul>
+              <RepositoryItem
+                repository={repository}
+              />
+
+              <RepositoryItem/>
+              <RepositoryItem/>
+              <RepositoryItem/>
+              <RepositoryItem/>
+              <RepositoryItem/>
+              <RepositoryItem/>
+              <RepositoryItem/>
+
+            </ul>
+        </section>
+    )
+}
